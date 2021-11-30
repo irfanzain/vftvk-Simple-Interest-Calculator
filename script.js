4e21cf12e521414c3548ca2de6a84e6033d3e77f
irfanzain <irfanzain.tech@gmail.com>
2021-11-30 07:10:58 +0000

    forth trial

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function compute()
 {
-    // p = document.getElementById("principal").value;
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     
+    //checking input amount
     if(principal <- 0) {
         alert("Enter a positive number");
 
@@ -16,7 +16,7 @@ function compute()
         return null;
     }
 
-    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
+    document.getElementById("result").innerHTML="If you deposit <mark>"+principal+"</mark>,\<br\>at an interest rate of <mark>"+rate+"%</mark>\<br\>You will receive an amount of <mark>"+interest+"</mark>,\<br\>in the year <mark>"+year+"</mark>\<br\>"
 
 }
 
